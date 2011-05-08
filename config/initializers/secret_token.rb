d68cdf45662b235b4357fef1c72f50d9d4e22c87
Darion Miller <darion@theservicecoach.com>
2011-05-07 22:03:47 -0400

    initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Referally::Application.config.secret_token = 'fa5ec02f7a7acb47162da296947637832f67a61fcb4a098f21902bc83a410d87fa4d1e799e15066e8b8c993410e8e569754d8c5a1fe12661555c859f8512fa8c'
